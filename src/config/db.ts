ca3d8ffb82e1468407eabffb88722d0c39bb8ad9
Alfaaz Ahmed <alfaazahmed010@gmail.com>
2026-08-11 12:34:02 +0600

    chore(db): initialize MongoDB connection

---
+++ src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose"
+
+export const connectDB = async () => {
+    try {
+        const mongoUri = process.env.MONGO_URI;
+        if (!mongoUri) {
+            throw new Error('MongoDB connection failed');
+        }
+        await mongoose.connect(mongoUri);
+        console.log('MongoDB connection successful');
+    }
+    catch (error) {
+        console.error('MongoDB connection failed', error);
+        process.exit(1);
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { connectDB } from "./config/db.js";
 dotenv.config();
 
-const app = express();
+const PORT = process.env.PORT || 5000;
+connectDB();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -13,7 +15,6 @@ app.get("/", (req, res) => {
     res.send("CareConnect Server is Working Properly!");
 });
 
-const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
